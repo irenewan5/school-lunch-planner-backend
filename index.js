b868c6c6d6414718e701af73aac56ba94a6306b0
Ling Wang <buthowever@gmail.com>
2024-05-02 22:07:36 -0300

    add routers to app

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require("express");
 require("dotenv/config");
 
+const plansRouter = require("./routes/plans");
 const recipesRouter = require("./routes/recipes");
+const usersRouter = require("./routes/users");
+const kidsRouter = require("./routes/kids");
+const tokenRouter = require("./routes/token");
 
 const app = express();
 
+app.use("/plans", plansRouter);
 app.use("/recipes", recipesRouter);
+app.use("/users", usersRouter);
+app.use("/kids", kidsRouter);
+app.use("/token", tokenRouter);
 
 app.listen(process.env.PORT, function () {
   console.log(`server is running at port ${process.env.PORT}`);
