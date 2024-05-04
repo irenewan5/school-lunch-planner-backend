de8bec0929477bb6d9f556ae0bb4efd0a8a267a9
Ling Wang <buthowever@gmail.com>
2024-05-04 11:33:44 -0300

    add endpoints for kids and user info

--- routes/kids.js
+++ routes/kids.js
@@ -10,18 +10,58 @@ router.get("/", async (req, res) => {
 });
 
 // Add a kid
-router.post("/", (req, res) => {
-  res.send({});
+router.post("/", async (req, res) => {
+  const { userId } = req.auth;
+  const [id] = await knex("kids").insert({
+    name: req.body.name,
+    user_id: userId,
+  });
+  const result = await knex("kids")
+    .where({
+      id,
+      user_id: userId,
+    })
+    .first();
+  res.send(result);
 });
 
-// Add a kid
-router.put("/:id", (req, res) => {
-  res.send({});
+// Update a kid
+router.put("/:id", async (req, res) => {
+  const { userId } = req.auth;
+  await knex("kids")
+    .update({
+      name: req.body.name,
+    })
+    .where({
+      id: req.params.id,
+      user_id: userId,
+    });
+
+  const result = await knex("kids")
+    .where({
+      id: req.params.id,
+      user_id: userId,
+    })
+    .first();
+
+  res.send(result);
 });
 
 // Delete a kid
-router.delete("/:id", (req, res) => {
-  res.send({});
+router.delete("/:id", async (req, res) => {
+  const { userId } = req.auth;
+  const kid = await knex("kids")
+    .where({
+      id: req.params.id,
+      user_id: userId,
+    })
+    .first();
+  await knex("kids").delete().where({
+    id: req.params.id,
+    user_id: userId,
+  });
+
+  res.send(kid);
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,40 @@
 const express = require("express");
 const router = express.Router();
+const knex = require("knex")(require("../knexfile"));
 
 // Register user
 router.post("/", (req, res) => {
   res.send({});
 });
 
+router.get("/me", async (req, res) => {
+  const result = await knex("users")
+    .where({
+      id: req.auth.userId,
+    })
+    .first();
+
+  res.send(result);
+});
+
 // Update profile
-router.put("/me", (req, res) => {
-  res.send({});
+router.put("/me", async (req, res) => {
+  await knex("users")
+    .update({
+      name: req.body.name,
+      email: req.body.email,
+    })
+    .where({
+      id: req.auth.userId,
+    });
+
+  const result = await knex("users")
+    .where({
+      id: req.auth.userId,
+    })
+    .first();
+
+  res.send(result);
 });
 
 module.exports = router;
